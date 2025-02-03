5b545fc56dc24dbd39b69041b4aff369b3918d1d
vovmomyt <volodya.artemev.1999@mail.ru>
2025-02-03 11:36:45 +0800

    'deploy'

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,8 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">УРААААА!!! Я люблю тебя, солнышко!!;))</div>
+          <div className="my-4 text-4xl font-bold">УРААААА!!! Я люблю тебя, солнышко!!</div>
+          <div className="text-2xl">ТЫ ЛУЧШАЯ!!!</div>
         </>
       ) : (
         <>
